50060b81450758033dd2f19d663fd32f29d2cc2b
Christian Fritz <mail@chr-fritz.de>
2017-01-22 20:44:52 +0100

    Finalizes functionality of the ApplyBrewSoftwareTask.

--- seuac-homebrew-plugin/src/main/groovy/de/qaware/seu/as/code/plugins/homebrew/ApplyBrewSoftwareTask.groovy
+++ seuac-homebrew-plugin/src/main/groovy/de/qaware/seu/as/code/plugins/homebrew/ApplyBrewSoftwareTask.groovy
@@ -3,33 +3,42 @@ package de.qaware.seu.as.code.plugins.homebrew
 import de.qaware.seu.as.code.plugins.base.DatastoreProvider
 import de.qaware.seu.as.code.plugins.base.DatastoreProviderFactory
 import de.qaware.seu.as.code.plugins.base.SeuacDatastore
+import org.gradle.api.DefaultTask
 import org.gradle.api.artifacts.Configuration
 import org.gradle.api.artifacts.Dependency
-import org.gradle.api.tasks.AbstractExecTask
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.TaskAction
+import org.gradle.process.internal.ExecAction
+import org.gradle.process.internal.ExecActionFactory
 
+import javax.inject.Inject
 import java.nio.file.Paths
-import java.util.stream.Collectors
 
 /**
+ * Applies the current home brew configuration within the seu.
+ *
+ * It will first uninstall removed brew software. Then it updates brew itself and any installed software.
+ * Finally it will install all new added brew software.
  *
  * @author christian.fritz
  */
-class ApplyBrewSoftwareTask extends AbstractExecTask {
-
+class ApplyBrewSoftwareTask extends DefaultTask {
     @Input
     Configuration source
-    //File target
     File homebrewBasePath
-    boolean withEmptyDirs = true
     SeuacDatastore datastore
 
-
+    /**
+     * Initializes the apply brew software task.
+     */
     ApplyBrewSoftwareTask() {
-        super(ApplyBrewSoftwareTask)
         group = 'SEU-as-code'
         description = 'Installs a homebrew package into the seu'
+    }
+
+    @Inject
+    protected ExecActionFactory getExecActionFactory() {
+        throw new UnsupportedOperationException();
     }
 
     /**
@@ -48,34 +57,68 @@ class ApplyBrewSoftwareTask extends AbstractExecTask {
         Set<String> obsoleteFiles = provider.findAllFiles(obsoleteDeps, source.name)
         uninstallOldPackages(obsoleteFiles)
 
+        updateBrewPackages()
+
         Set<Dependency> incomingDeps = provider.findAllIncomingDeps(source.dependencies, source.name)
-        installNewPackages(incomingDeps)
+        installNewPackages(incomingDeps, provider)
     }
 
+    /**
+     * Updates homebrew it self and then updates all installed home brew software.
+     */
+    def updateBrewPackages() {
+        def update = createBrewCommand()
+        update.commandLine += 'update'
+        update.execute()
+
+        def upgrade = createBrewCommand()
+        upgrade.commandLine += 'upgrade'
+        upgrade.execute()
+    }
+
+    /**
+     * Removes the given brew packages.
+     *
+     * @param uninstallDeps a set of home brew package names to remove
+     */
     def uninstallOldPackages(Set<String> uninstallDeps) {
         if (uninstallDeps.isEmpty()) {
             return
         }
 
-        def uninstall = getExecActionFactory().newExecAction()
-        uninstall.workingDir = homebrewBasePath
-
-        uninstall.commandLine(Paths.get(homebrewBasePath.path, 'bin', 'brew').toString(), 'uninstall')
+        def uninstall = createBrewCommand()
+        uninstall.commandLine += 'uninstall'
         uninstall.commandLine += uninstallDeps
 
         def execute = uninstall.execute()
         execute.rethrowFailure()
     }
 
+    /**
+     * Installs the given set of dependencies using brew and store them within the seu management database using
+     * the given provider.
+     *
+     * @param dependencies The dependencies to install.
+     * @param provider The provider to store the installed dependencies in
+     */
+    def installNewPackages(Set<Dependency> dependencies, DatastoreProvider provider) {
+        dependencies.forEach({ d ->
+            def installTool = createBrewCommand()
+            installTool.commandLine += ['install', d.name]
+            installTool.execute()
+            provider.storeDependency(d, null, 'brew')
+        })
+    }
 
-    def installNewPackages(Set<Dependency> dependencies) {
-        workingDir = homebrewBasePath
-
-        commandLine(Paths.get(homebrewBasePath.path, 'bin', 'brew').toString(), 'install')
-        commandLine += dependencies.stream()
-                .map({ d -> d.name })
-                .collect(Collectors.toSet())
-
-        super.exec()
+    /**
+     * Create a new {@link ExecAction} for the brew installation in the current seu.
+     *
+     * @return A new exec action.
+     */
+    protected ExecAction createBrewCommand() {
+        def action = getExecActionFactory().newExecAction()
+        action.commandLine(Paths.get(homebrewBasePath.path, 'bin', 'brew').toString())
+        action.workingDir = homebrewBasePath
+        return action
     }
 }
